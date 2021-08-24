66c90c87a737cafb4f9e86f11a5d41c2afb725b2
techguytechtips <vance_peever@hotmail.com>
2021-08-23 20:35:58 -0400

    changed name, turned number finding code into function and fixed issue if file had no number after delimiter

--- Renumberrus.py
+++ Renumberrus.py
@@ -3,30 +3,36 @@ import os
 folder = input("Enter folder name: ")
 delimiter = input("Enter delimiter: ")
 
-# for every file name
-for filename in os.listdir(folder):
-    
+def getNumbers(filename):
+    numbers = ""
     # get every character after the delimiter
     try:
         ending = filename.split(delimiter,1)[1]
-    except IndexError as e:
-        print("Failed to rename: " + filename + "\nBecause delimter condition does not exist.")
-        continue
-    numbers = ""
-    finalname = ""
-    
+    except IndexError:
+       return numbers
+
     # for every character in the rest of the file name
     for char in ending:
         # check if it is a number
         # if it is add it to the numbers var
-        # otherwise just break out of the for loop entirely
+        # otherwise just break out of the for loop entirely 
         if char.isnumeric():
             numbers += char
         else:
             break
-    
-    print("Renamed: " + filename)
+    return numbers
 
+
+# for every file name
+for filename in os.listdir(folder):
+    
+    numbers = getNumbers(filename)
+    if (numbers == ""):
+        print("Failed to rename: " + filename + "\nbecause delimter condition does not exist")
+        print("or because there is no numbers after delimiter.")
+        continue
+
+    print("Renamed: " + filename)
     # create the final name for the file
     # which will be the filename with the number
     # of the file at the beginning in brackets
